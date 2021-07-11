609a47f64ba3d7803c0f3bc9d62e850dc43bcf66
grego33 <mattgregory33@gmail.com>
2021-07-11 08:57:15 -0400

    Moved EE_HANDS definition to keyboard config.

--- keyboards/sofle/keymaps/keyhive_via/config.h
+++ keyboards/sofle/keymaps/keyhive_via/config.h
@@ -22,8 +22,6 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-#define EE_HANDS
-
 //Add RGB underglow
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 74
--- keyboards/sofle/rev2/config.h
+++ keyboards/sofle/rev2/config.h
@@ -7,7 +7,7 @@
 #define MANUFACTURER    Keyhive
 #define PRODUCT         Sharon
 #define DESCRIPTION     Rose version of the Sofle
-// #define MASTER_RIGHT
+#define EE_HANDS
 
 /* key matrix size */
 // Rows are doubled-up
