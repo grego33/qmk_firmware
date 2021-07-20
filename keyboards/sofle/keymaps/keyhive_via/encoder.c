798d76c0a0c3271ed523a8ffbc6e494689e7e3cd
grego33 <mattgregory33@gmail.com>
2021-07-19 21:31:31 -0400

    Swapped encoder behavior L/R

--- keyboards/sofle/keymaps/keyhive_via/encoder.c
+++ keyboards/sofle/keymaps/keyhive_via/encoder.c
@@ -22,15 +22,15 @@
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    } else if (index == 1) {
-        if (clockwise) {
             tap_code16(LCTL(KC_RIGHT));
         } else {
             tap_code16(LCTL(KC_LEFT));
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
         }
     }
 }
